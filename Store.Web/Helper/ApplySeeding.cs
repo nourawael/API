fafb00e3da773f8852436cd1b8a6c5a28b33b9ea
nourawael <nourawael99@gmail.com>
2024-09-24 14:38:00 +0300

    Create databse if does not alreday exsist

--- Store.Web/Helper/ApplySeeding.cs
+++ Store.Web/Helper/ApplySeeding.cs
@@ -1,4 +1,5 @@
-﻿using Store.Data.Contexts;
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Data.Contexts;
 using Store.Repository;
 
 namespace Store.Web.Helper
@@ -14,6 +15,7 @@ namespace Store.Web.Helper
                 try
                 {
                     var context = services.GetRequiredService<StoreDbContext>();
+                    await context.Database.MigrateAsync();
                     await StoreContextSeed.SeedAsync(context, logerFactory);
                 }
                 catch (Exception ex)
