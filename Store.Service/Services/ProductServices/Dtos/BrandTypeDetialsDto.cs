b3a2b869fe4d7f3ba4d88a5f9bee797b0f3536e5
nourawael <nourawael99@gmail.com>
2024-09-24 17:05:49 +0300

    Apply AutoMapper on ProductService

--- Store.Service/Services/ProductServices/Dtos/BrandTypeDetialsDto.cs
+++ Store.Service/Services/ProductServices/Dtos/BrandTypeDetialsDto.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Store.Service.Services.Product.Dtos
+namespace Store.Service.Services.ProductServices.Dtos
 {
     public class BrandTypeDetialsDto
     {
--- Store.Service/Services/ProductServices/IProductService.cs
+++ Store.Service/Services/ProductServices/IProductService.cs
@@ -1,11 +1,11 @@
-﻿using Store.Service.Services.Product.Dtos;
+﻿using Store.Service.Services.ProductServices.Dtos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Store.Service.Services
+namespace Store.Service.ProductServices
 {
     public interface IProductService
     {
