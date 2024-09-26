29c888b20edc7182dbbbc810b04a7f6985652b94
nourawael <nourawael99@gmail.com>
2024-09-26 14:38:23 +0300

    Product Specifications

---
+++ Store.Repository/Specification/ProductSpec/ProductSpecification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Repository.Specification.ProductSpec
+{
+    public class ProductSpecification
+    {
+        public int? BrandId { get; set; }
+        public int? TypeId { get; set; }
+    }
+}
