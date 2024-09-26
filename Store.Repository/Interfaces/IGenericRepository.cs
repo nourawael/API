5fb3d978402acc7f26d427fb155cbbe20093f2ea
nourawael <nourawael99@gmail.com>
2024-09-26 14:26:01 +0300

    Build Specification Evaluator

--- Store.Repository/Interfaces/IGenericRepository.cs
+++ Store.Repository/Interfaces/IGenericRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Store.Data.Entities;
+using Store.Repository.Specification;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +11,10 @@ namespace Store.Repository.Interfaces
     public interface IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
     {
         Task<TEntity> GetByIdAsync(TKey? id);
-        //Task<TEntity> GetByIdAsNoTrackingAsync(TKey? id);
         Task<IReadOnlyList<TEntity>> GetAllAsync();
         Task<IReadOnlyList<TEntity>> GetAllAsNoTrackingAsync();
-
+        Task<TEntity> GetWithSpecificationByIdAsync(ISpecification<TEntity> specs);
+        Task<IReadOnlyList<TEntity>> GetAllWithSpecificationAsync(ISpecification<TEntity> specs);
         Task AddAsync(TEntity entity);
         void Update(TEntity entity);
         void Delete(TEntity entity);
