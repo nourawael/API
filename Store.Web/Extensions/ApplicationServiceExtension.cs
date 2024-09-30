cca1dc95e4b14acdf149d7b2baa8938dc34ae5a1
nourawael <nourawael99@gmail.com>
2024-09-30 17:02:38 +0300

    Refactoring Program Class

---
+++ Store.Web/Extensions/ApplicationServiceExtension.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Repository.Interfaces;
+using Store.Repository.Repositories;
+using Store.Service.HandleResponses;
+using Store.Service.ProductServices;
+using Store.Service.Services.ProductServices.Dtos;
+
+namespace Store.Web.Extensions
+{
+    public static class ApplicationServiceExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services) 
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddAutoMapper(typeof(ProductProfile));
+
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = actionConext =>
+                {
+                    var errors = actionConext.ModelState
+                    .Where(model => model.Value?.Errors.Count > 0)
+                    .SelectMany(model => model.Value?.Errors)
+                    .Select(error => error.ErrorMessage)
+                    .ToList();
+
+                    var errorResponse = new ValidationErrorResponse
+                    {
+                        Errors = errors
+                    };
+
+                    return new BadRequestObjectResult(errorResponse);
+
+                };
+            });
+            return services;
+        }
+    }
+}
