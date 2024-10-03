fe6a523a65c11403f328edc4e642fa0113384622
nourawael <nourawael99@gmail.com>
2024-10-03 16:49:06 +0300

    Start wit identity (tables & migrations)

---
+++ Store.Data/Contexts/StoreIdenitityDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Store.Data.Entities.IdentityEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Data.Contexts
+{
+    public class StoreIdenitityDbContext : IdentityDbContext<AppUser>
+    {
+        public StoreIdenitityDbContext(DbContextOptions<StoreIdenitityDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ Store.Web/Extensions/IdentityServiceExtension.cs
@@ -0,0 +1,23 @@
+﻿using Store.Data.Entities.IdentityEntities;
+using Microsoft.AspNetCore.Identity;
+using Store.Data.Contexts;
+namespace Store.Web.Extensions
+{
+    public static class IdentityServiceExtension
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services) 
+        {
+            var builder = services.AddIdentityCore<AppUser>();
+
+            builder = new IdentityBuilder(builder.UserType, builder.Services);
+
+            builder.AddEntityFrameworkStores<StoreIdenitityDbContext>();
+
+            builder.AddSignInManager<SignInManager<AppUser>>();
+
+            services.AddAuthentication();
+
+            return services;
+        }
+    }
+}
