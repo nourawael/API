9603ac6449b2a8a2daf3014990313ea354acf897
nourawael <nourawael99@gmail.com>
2024-10-09 14:45:15 +0300

    Account Controller (Get Current user Details)

--- Store.Web/Controllers/AccountController.cs
+++ Store.Web/Controllers/AccountController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Store.Data.Entities.IdentityEntities;
 using Store.Service.HandleResponses;
 using Store.Service.Services.UserService;
 using Store.Service.Services.UserService.Dtos;
@@ -10,10 +13,12 @@ namespace Store.Web.Controllers
     public class AccountController : BaseController
     {
         private readonly IUserService _userService;
+        private readonly UserManager<AppUser> _userManager;
 
-        public AccountController(IUserService userService)
+        public AccountController(IUserService userService, UserManager<AppUser> userManager)
         {
             _userService = userService;
+            _userManager = userManager;
         }
 
         [HttpPost]
@@ -37,6 +42,23 @@ namespace Store.Web.Controllers
                 return BadRequest(new CustomException(400, "Email already exists"));
 
             return Ok(user);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<UserDto> GetCurrentUserDetails()
+        {
+            var userId = User?.FindFirst("UserId");
+
+            var user = await _userManager.FindByIdAsync(userId.Value);
+
+            return new UserDto
+            {
+                Id = Guid.Parse(user.Id),
+                DisplayName = user.DipslayName,
+                Email = user.Email,
+            };
+
         }
     }
 }
