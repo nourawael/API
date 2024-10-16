867f05ea2cdf8467bf3753b8a3cfce7f4206fbcd
nourawael <nourawael99@gmail.com>
2024-10-16 14:23:10 +0300

    test full cycle for creating order

--- Store.Web/Extensions/ApplicationServiceExtension.cs
+++ Store.Web/Extensions/ApplicationServiceExtension.cs
@@ -26,8 +26,8 @@ namespace Store.Web.Extensions
             services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserService, UserService>();
-            //services.AddScoped<IOrderService, OrderService>();
-            //services.AddScoped<IPaymentService, PaymentService>();
+            services.AddScoped<IOrderService, OrderService>();
+            services.AddScoped<IPaymentService, PaymentService>();
 
             services.AddAutoMapper(typeof(ProductProfile));
             services.AddAutoMapper(typeof(BasketProfile));
